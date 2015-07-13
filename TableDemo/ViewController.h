b0eee4c373b2050cd0c6ffecad093d30a35b7a2b
ammar <ammar@Ammars-iMac-2.local>
2015-07-13 12:38:38 +0500

    Working code

--- TableDemo/ViewController.h
+++ TableDemo/ViewController.h
@@ -8,7 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+
+@property IBOutlet UITableView *table;
+@property (strong, nonatomic) IBOutlet UIImageView *imgv;
+
 
 
 @end
